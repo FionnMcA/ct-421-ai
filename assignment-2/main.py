97de7a660255c746224cbcd858bd43c91e8bb799
Fionn McArdle <f.mcardle1@universityofgalway.ie>
2025-02-26 13:45:22 +0000

    print total results

--- assignment-2/main.py
+++ assignment-2/main.py
@@ -12,6 +12,13 @@ def play_all_strategies():
         TitForTat()
     ]
 
+    total_results = {
+        strategy.get_name(): {
+        'total_points': 0
+        }
+        for strategy in strategies
+    }
+
     rounds = 10
 
     for strat1, strat2 in product(strategies, repeat=2):
@@ -19,6 +26,11 @@ def play_all_strategies():
         p1_points, p2_points = pd.play_game()
 
         print(f"{strat1.get_name()} - {p1_points} vs {strat2.get_name()} = {p2_points}")
+
+        total_results[strat1.get_name()]['total_points'] += p1_points
+        total_results[strat2.get_name()]['total_points'] += p2_points
+
+    print(total_results)
 
 if __name__ == '__main__':
     play_all_strategies()
